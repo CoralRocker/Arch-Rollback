cfd4134dd654fbad1f4b3151595343bda8f22bda
Gaultier Delbarre <gaultiertexas@gmail.com>
2020-01-31 08:48:58 -0600

    Getting multi-selection to work isn't working

--- SelectPackage.py
+++ SelectPackage.py
@@ -14,6 +14,11 @@ def main(stdscr):
     l = pacman.pacman_list()
     l.getCachePackages()
     l.sortCachePackages() 
+    
+    curses.start_color()
+    curses.use_default_colors()
+    curses.init_pair(1, curses.COLOR_RED, -1)
+    curses.init_pair(2, curses.COLOR_GREEN, -1)
 
     current_key = 0
     max_key = len(l.alphabetised.keys())
@@ -26,11 +31,17 @@ def main(stdscr):
         pkg_list = l.alphabetised[key]
         num_items = len(pkg_list)
         offset = 0
+        multiselect_indeces = dict()
+        for k in l.alphabetised.keys():
+            multiselect_indeces[k] = []
+        space_len = len(str(len(pkg_list)))
         for index, pkg in enumerate(pkg_list, 1):
             if index == height:
                 break
-            stdscr.addstr(index + offset, 0, str(pkg), (curses.A_REVERSE if current_item == index-1 else 0))
-        stdscr.refresh()
+            stdscr.addstr(index + offset, 0, "("+(" "*(space_len-len(str(index))))+str(index)+") ", curses.color_pair(1)) # Gets correctly formatted index
+            stdscr.addstr(str(pkg), (curses.A_REVERSE if current_item == index-1 or current_item in multiselect_indeces[key] else 0))
+        
+        stdscr.addstr(height - 1, 0, str(multiselect_indeces))
         c = stdscr.getch()
         if chr(c).lower() == 'q':
             break
@@ -44,5 +55,12 @@ def main(stdscr):
             current_item = (current_item + 1 if current_item + 1 < num_items else 0)
         elif c == curses.KEY_UP:
             current_item = (current_item - 1 if current_item - 1 >= 0 else num_items - 1)
+        elif c == curses.KEY_ENTER:
+            if current_item in multiselect_indeces[key]:
+                multiselect_indeces[key].remove(current_item)
+            else:
+                multiselect_indeces[key].append(current_item)
+        stdscr.refresh()
+
 
 curses.wrapper(main)
