1145b217c83e481281b0d752638eda09625f66bf
Gaultier Delbarre <gaultiertexas@gmail.com>
2020-01-31 10:08:29 -0600

    Im the king of stupid bugs. Bow down, peasant

--- SelectPackage.py
+++ SelectPackage.py
@@ -23,6 +23,9 @@ def main(stdscr):
     current_key = 0
     max_key = len(l.alphabetised.keys())
     current_item = 0
+    multiselect_indeces = dict()
+    for k in l.alphabetised.keys():
+        multiselect_indeces[k] = []
     while True:
         stdscr.clear()
         key = list(l.alphabetised.keys())[current_key]
@@ -31,17 +34,14 @@ def main(stdscr):
         pkg_list = l.alphabetised[key]
         num_items = len(pkg_list)
         offset = 0
-        multiselect_indeces = dict()
-        for k in l.alphabetised.keys():
-            multiselect_indeces[k] = []
         space_len = len(str(len(pkg_list)))
         for index, pkg in enumerate(pkg_list, 1):
             if index == height:
                 break
             stdscr.addstr(index + offset, 0, "("+(" "*(space_len-len(str(index))))+str(index)+") ", curses.color_pair(1)) # Gets correctly formatted index
-            stdscr.addstr(str(pkg), (curses.A_REVERSE if current_item == index-1 or current_item in multiselect_indeces[key] else 0))
+            stdscr.addstr(str(pkg), (curses.A_REVERSE if current_item == index-1 else 0)|(curses.A_REVERSE if index-1 in multiselect_indeces[key] else 0))
         
-        stdscr.addstr(height - 1, 0, str(multiselect_indeces))
+        stdscr.addstr(height - 1, 0, str(multiselect_indeces[key]))
         c = stdscr.getch()
         if chr(c).lower() == 'q':
             break
@@ -55,11 +55,8 @@ def main(stdscr):
             current_item = (current_item + 1 if current_item + 1 < num_items else 0)
         elif c == curses.KEY_UP:
             current_item = (current_item - 1 if current_item - 1 >= 0 else num_items - 1)
-        elif c == curses.KEY_ENTER:
-            if current_item in multiselect_indeces[key]:
-                multiselect_indeces[key].remove(current_item)
-            else:
-                multiselect_indeces[key].append(current_item)
+        elif chr(c) == 'e':
+            multiselect_indeces[key].append(current_item)
         stdscr.refresh()
 
 
